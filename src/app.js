a79ee8995305fd5880785ba52866919cb789ff64
Akarsh Chaturvedi <akarshchaturvedi2555@gmail.com>
2025-07-05 00:30:06 +0530

    utils code

---
+++ src/app.js
@@ -0,0 +1,30 @@
+import express from 'express'
+import cors from 'cors'
+import cookieParser from 'cookie-parser'
+// cookie parser is use to access and set cokkies on browser
+
+const app = express()
+
+// handeling CORS - middleware syntax- app.use()
+app.use(cors({
+    origin: process.env.CORS_ORIGIN,
+    credentials: true
+}))
+
+// config from incomming json data
+app.use(express.json({
+    limit: "16kb"
+}))
+
+//cofig for incomming url data
+app.use(express.urlencoded({
+    extended: true,
+    limit: "16kb"
+}))
+
+//config for static
+app.use(express.static())
+
+app.use(cookieParser())
+
+export {app}
